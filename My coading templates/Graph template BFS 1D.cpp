7483b21cc737e2f79d09267e75bf90bb965cbbd2
yasirnabil534 <31351666+yasirnabil534@users.noreply.github.com>
2017-12-28 02:06:51 +0600

    Add files via upload

---
+++ My coading templates/Graph template BFS 1D.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+
+#define ll              long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define pb(a)           push_back(a)
+#define mp              make_pair
+#define db              double
+#define EPS             10E-10
+#define ff              first
+#define ss              second
+#define sqr(x)          (x)*(x)
+#define vi              vector< int >
+#define vl              vector< ll >
+#define vii             vector<vector< int > >
+#define vll             vector<vector< ll > >
+#define vpii            vector< pii >
+#define vpll            vector< pll >
+#define DBG             pf("HI\n")
+#define MOD             1000000007
+#define CIN             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define RUN_CASE(t,T)   for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)         printf("Case %d: ",t)
+#define CASEL(t)        printf("Case %d:\n",t)
+#define intlimit        2147483648
+#define longlimit       9223372036854775808
+#define infinity        (1<<28)
+#define gcd(a, b)       __gcd(a,b)
+#define lcm(a, b)       ((a)*(b)/gcd(a,b))
+#define mx              123456789
+
+using namespace std;
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fx[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+vi graph[10005];
+int visited[10005];
+
+void make_graph_list(int u, int v)
+{
+    graph[u].pb(v);
+}
+
+void BFS(int source)
+{
+    queue <int> Q;
+    Q.push(source);
+    visited[source] = 1;
+    while(!Q.empty())
+    {
+        int u = Q.front();
+        Q.pop();
+        for(int i=0;i<graph[u].size();++i)
+        {
+            if(visited[graph[u][i]]==0)
+            {
+                int v = graph[u][i];
+                visited[v] = 1;
+                Q.push(v);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
