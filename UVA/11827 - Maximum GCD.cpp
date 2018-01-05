4ac689ad304a1a6de58dcc2900dcaa538b4c6c12
yasirnabil534 <31351666+yasirnabil534@users.noreply.github.com>
2018-01-05 07:49:16 -0800

    Add files via upload

---
+++ UVA/11827 - Maximum GCD.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+
+#define ll              long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define pb(a)           push_back(a)
+#define mp              make_pair
+#define db              double
+#define EPS             10E-10
+#define ff              first
+#define ss              second
+#define sqr(x)          (x)*(x)
+#define vi              vector< int >
+#define vl              vector< ll >
+#define vii             vector<vector< int > >
+#define vll             vector<vector< ll > >
+#define DBG             pf("HI\n")
+#define MOD             1000000007
+#define CIN             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define RUN_CASE(t,T)   for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)         printf("Case %d: ",t)
+#define CASEL(t)        printf("Case %d:\n",t)
+#define intlimit        2147483648
+#define longlimit       9223372036854775808
+#define infinity        (1<<28)
+#define gcd(a, b)       __gcd(a,b)
+#define lcm(a, b)       ((a)*(b)/gcd(a,b))
+#define mx              123456789
+#define PI              2*acos(0.0)
+#define rep(i,a,b)      for(__typeof(i) i=a; i<=b; i++)
+#define rev(i,a,b)      for(__typeof(i) i=a; i>=b; i--)
+
+
+using namespace std;
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+bool cmp(int a,int b)
+{
+    return a>b;
+}
+
+int main()
+{
+    int t,T;
+    scin(T);
+    getchar();
+    RUN_CASE(t,T)
+    {
+        string str;
+        stringstream ss;
+        int ar[105], key=0, inp, Max = -1;
+        getline(cin,str);
+        ss<<str;
+        while(ss>>inp)
+        {
+            ar[key++] = inp;
+        }
+        sort(ar,ar+key);
+        for(int i=1;i<key;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                inp = gcd(ar[j],ar[i]);
+                if(inp>Max)
+                    Max = inp;
+            }
+        }
+        cout<<Max<<endl;
+    }
+    return 0;
+}
