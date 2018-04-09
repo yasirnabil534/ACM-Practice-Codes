ca3423d3365ead2ed86c7c933e6afb6ef667f4f0
Yasir Uddin Ahamed (Nabil) <31351666+yasirnabil534@users.noreply.github.com>
2018-04-10 01:41:58 +0600

    Add files via upload

---
+++ LightOJ/Project LightOJ 1189.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+
+#define ll              long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define pb(a)           push_back(a)
+#define mp              make_pair
+#define db              double
+#define EPS             10E-10
+#define ff              first
+#define ss              second
+#define sqr(x)          (x)*(x)
+#define vi              vector< int >
+#define vl              vector< ll >
+#define vii             vector<vector< int > >
+#define vll             vector<vector< ll > >
+#define DBG             pf("HI\n")
+#define MOD             1000000007
+#define CIN             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define RUN_CASE(t,T)   for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)         printf("Case %d: ",t)
+#define CASEL(t)        printf("Case %d:\n",t)
+#define intlimit        2147483648
+#define longlimit       9223372036854775808
+#define infinity        (1<<28)
+#define gcd(a, b)       __gcd(a,b)
+#define lcm(a, b)       ((a)*(b)/gcd(a,b))
+#define mx              1234567899
+#define PI              2*acos(0.0)
+#define rep(i,a,b)      for(__typeof(i) i=a; i<=b; i++)
+#define rev(i,a,b)      for(__typeof(i) i=a; i>=b; i--)
+
+
+using namespace std;
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+vector<ll>vec,ans;
+
+int main()
+{
+    vec.pb(1);
+    vec.pb(1);
+    ll m = 1;
+    for(ll i = 2; i < 21; i++)
+    {
+        m*=(i);
+        vec.pb(m);
+    }
+    reverse(vec.begin(),vec.end());
+    ll t,T;
+    scln(T);
+    RUN_CASE(t,T)
+    {
+        ll n;
+        scln(n);
+        for(int i = 0; i < 21; i++)
+        {
+            if(vec[i]<=n)
+            {
+                ans.pb(20-i);
+                n-=vec[i];
+            }
+        }
+        sort(ans.begin(),ans.end());
+        CASE(t);
+        if(n>0)
+            pf("impossible");
+        else
+        for(int i = 0; i < ans.size(); i++)
+        {
+            if(i>0)
+                pf("+");
+            pf("%lld!",ans[i]);
+        }
+        pf("\n");
+        ans.clear();
+    }
+    return 0;
+}
