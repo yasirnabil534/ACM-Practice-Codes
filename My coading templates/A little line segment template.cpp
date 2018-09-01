5880156923c32a50ae8a450e5d0b422eb377ab40
Yasir Uddin Ahamed (Nabil) <31351666+yasirnabil534@users.noreply.github.com>
2018-09-02 01:16:30 +0600

    Update A little line segment template.cpp

--- My coading templates/A little line segment template.cpp
+++ My coading templates/A little line segment template.cpp
@@ -74,6 +74,15 @@ template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_
 ///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
 ///---------------------------------------------------------------
 
+bool onSegment(Point p, Point q, Point r)
+{
+    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
+        q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
+       return true;
+ 
+    return false;
+}
+
 int orientation(pll p, pll q, pll r)
 {
     // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
@@ -95,6 +104,18 @@ bool det(pll p1,pll q1,pll p2, pll q2)
     // General case
     if (o1 != o2 && o3 != o4)
         return true;
+    // Special Cases
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+ 
+    // p1, q1 and q2 are colinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+ 
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+ 
+     // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
     return false;
 }
 
