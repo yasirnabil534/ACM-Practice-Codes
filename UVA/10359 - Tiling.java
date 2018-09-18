382246f905b97350df705087a63dc540ef3d06af
Yasir Uddin Ahamed (Nabil) <31351666+yasirnabil534@users.noreply.github.com>
2018-09-18 06:36:43 +0600

    Update 10359 - Tiling.java

--- UVA/10359 - Tiling.java
+++ UVA/10359 - Tiling.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package solution;
-
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -12,7 +5,7 @@ import java.util.Scanner;
  *
  * @author Nabil
  */
-public class Solution {
+public class Main {
     public static Scanner sc = new Scanner(System.in);
     public static BigInteger TWO = new BigInteger("2");
     public static BigInteger dp[] = new BigInteger[300];
