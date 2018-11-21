c19844083672fd78c3c142fd590d363f4e0ab472
Yasir Uddin Ahamed (Nabil) <31351666+yasirnabil534@users.noreply.github.com>
2018-11-21 23:00:12 +0600

    Add files via upload

--- UVA/11827 - Maximum GCD.cpp
+++ UVA/11827 - Maximum GCD.cpp
@@ -31,13 +31,34 @@
 #define infinity        (1<<28)
 #define gcd(a, b)       __gcd(a,b)
 #define lcm(a, b)       ((a)*(b)/gcd(a,b))
-#define mx              123456789
+#define mx              1234567899
 #define PI              2*acos(0.0)
+#define all(v)          v.begin(),v.end()
 #define rep(i,a,b)      for(__typeof(i) i=a; i<=b; i++)
 #define rev(i,a,b)      for(__typeof(i) i=a; i>=b; i--)
 
-
 using namespace std;
+
+template<typename T>inline T POW(T B,T P){ if(P==0) return 1; if(P&1) return B*POW(B,P-1);  else return sqr(POW(B,P/2));}
+ll Bigmod(ll base, ll power, ll mod){if(power==0) return 1%mod; if(power==1) return base%mod; ll x = Bigmod(base,power/2,mod); x = (x*x)%mod; if(power%2){x = (x*(base%mod))%mod;}return x;}
+ll Mulmod(ll base, ll power, ll mod){if(power==0) return 0; if(power==1) return base%mod; ll x = Mulmod(base,power/2,mod); x = (x+x)%mod; if(power%2){x = (x+(base%mod))%mod;}return x;}
+bool isVowel(char ch){ ch=toupper(ch); if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E') return true; return false;}
+ll ModInverse(ll number, ll mod){return Bigmod(number, mod-2, mod);}
+bool isConso(char ch){if (isalpha(ch) && !isVowel(ch)) return true; return false;}
+int toInt(string s)  { int sm; stringstream ss(s); ss>>sm; return sm; }
+bool isPrime( ll val ) { if( val == 2 ) return true ; if( val % 2 == 0 || val == 1 ) return false ; ll sqrt_N = (ll) ( ( double ) sqrt( val ) ) ; for( ll i = 3 ; i <= sqrt_N ; i += 2 ) { if( val % i == 0 ) return false ; } return true ; }
+template<class T> string convert(T _input) { stringstream blah; blah << _input; return blah.str(); }
+
+/**
+// Ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(1)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+*/
 
 ///--------------Graph Moves--------------------------------------
 ///const int fx[] = {+1,-1,+0,+0};
@@ -55,31 +76,27 @@ bool cmp(int a,int b)
 
 int main()
 {
-    int t,T;
-    scin(T);
+    ll n,t,x;
+    scln(t);
     getchar();
-    RUN_CASE(t,T)
+    while(t--)
     {
         string str;
-        stringstream ss;
-        int ar[105], key=0, inp, Max = -1;
+        vl vc;
         getline(cin,str);
-        ss<<str;
-        while(ss>>inp)
+        stringstream ss(str);
+        while(ss>>x)
         {
-            ar[key++] = inp;
+            vc.pb(x);
         }
-        sort(ar,ar+key);
-        for(int i=1;i<key;i++)
+        n = (ll)vc.size();
+        x = 0;
+        for(ll i = 0; i < n; i++)
         {
-            for(int j=0;j<i;j++)
-            {
-                inp = gcd(ar[j],ar[i]);
-                if(inp>Max)
-                    Max = inp;
-            }
+            for(ll j = i + 1; j < n; j++)
+                x = max(x,gcd(vc[i],vc[j]));
         }
-        cout<<Max<<endl;
+        pf("%lld\n",x);
     }
     return 0;
 }
