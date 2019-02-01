ae9a9cbf72d4fac96858a1aff1c30d1e118392bd
Yasir Uddin Ahamed (Nabil) <31351666+yasirnabil534@users.noreply.github.com>
2019-02-01 15:53:11 +0600

    Add files via upload

---
+++ UVA/793 - Network Connections.cpp
@@ -0,0 +1,158 @@
+#include<bits/stdc++.h>
+
+#define ll              long long int
+#define ull             unsigned long long int
+#define pii             pair < int, int>
+#define pll             pair < ll, ll>
+#define sc              scanf
+#define scin(x)         sc("%d",&(x))
+#define scln(x)         sc("%lld",&(x))
+#define pf              printf
+#define ms(a,b)         memset(a,b,sizeof(a))
+#define pb(a)           push_back(a)
+#define mp              make_pair
+#define db              double
+#define EPS             10E-10
+#define ff              first
+#define ss              second
+#define sqr(x)          (x)*(x)
+#define vi              vector< int >
+#define vl              vector< ll >
+#define vii             vector<vector< int > >
+#define vll             vector<vector< ll > >
+#define DBG             pf("HI\n")
+#define MOD             1000000007
+#define CIN             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define RUN_CASE(t,T)   for(__typeof(t) t=1;t<=T;t++)
+#define CASE(t)         printf("Case %d: ",t)
+#define CASEL(t)        printf("Case %d:\n",t)
+#define intlimit        2147483648
+#define longlimit       9223372036854775808
+#define infinity        (1<<28)
+#define gcd(a, b)       __gcd(a,b)
+#define lcm(a, b)       ((a)*(b)/gcd(a,b))
+#define mx              1234567899
+#define PI              2*acos(0.0)
+#define all(v)          v.begin(),v.end()
+#define rep(i,a,b)      for(__typeof(i) i=a; i<=b; i++)
+#define rev(i,a,b)      for(__typeof(i) i=a; i>=b; i--)
+
+using namespace std;
+
+template<typename T>inline T POW(T B,T P){ if(P==0) return 1; if(P&1) return B*POW(B,P-1);  else return sqr(POW(B,P/2));}
+ll Bigmod(ll base, ll power, ll mod){if(power==0) return 1%mod; if(power==1) return base%mod; ll x = Bigmod(base,power/2,mod); x = (x*x)%mod; if(power%2){x = (x*(base%mod))%mod;}return x;}
+ll Mulmod(ll base, ll power, ll mod){if(power==0) return 0; if(power==1) return base%mod; ll x = Mulmod(base,power/2,mod); x = (x+x)%mod; if(power%2){x = (x+(base%mod))%mod;}return x;}
+ll BigMulmod(ll base, ll power, ll mod){if(power==0) return 1%mod; if(power==1) return base%mod; ll x = BigMulmod(base,power/2,mod); x = Mulmod(x,x,mod); if(power%2){x = Mulmod(x,(base%mod),mod);}return x;}
+bool isVowel(char ch){ ch=toupper(ch); if(ch=='A'||ch=='U'||ch=='I'||ch=='O'||ch=='E') return true; return false;}
+ll ModInverse(ll number, ll mod){return Bigmod(number, mod-2, mod);}
+bool isConso(char ch){if (isalpha(ch) && !isVowel(ch)) return true; return false;}
+int toInt(string s)  { int sm; stringstream ss(s); ss>>sm; return sm; }
+bool isPrime( ll val ) { if( val == 2 ) return true ; if( val % 2 == 0 || val == 1 ) return false ; ll sqrt_N = (ll) ( ( double ) sqrt( val ) ) ; for( ll i = 3 ; i <= sqrt_N ; i += 2 ) { if( val % i == 0 ) return false ; } return true ; }
+template<class T> string convert(T _input) { stringstream blah; blah << _input; return blah.str(); }
+
+/**
+// Ordered set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T> using orderset = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+//orderset<int> s ; //orderset<int>::iterator it ;
+//orderset<int> X; //X.insert(1); //X.insert(2); //X.insert(4); //X.insert(8); //X.insert(16);
+//cout<<*X.find_by_order(1)<<endl; // 2 //cout<<*X.find_by_order(2)<<endl; // 4 //cout<<*X.find_by_order(4)<<endl; // 16 //cout<<(end(X)==X.find_by_order(6))<<endl; // true
+//cout<<X.order_of_key(-5)<<endl;  // 0 //cout<<X.order_of_key(1)<<endl;   // 0 //cout<<X.order_of_key(3)<<endl;   // 2 //cout<<X.order_of_key(4)<<endl;   // 2 //cout<<X.order_of_key(400)<<endl; // 5
+*/
+
+///--------------Graph Moves--------------------------------------
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+///---------------------------------------------------------------
+
+bool cmp(int a,int b)
+{
+    return a>b;
+}
+
+int par[10005];
+int _rank[10005];
+
+int _find(int u)
+{
+    if(par[u]==u)
+        return u;
+    return par[u] = _find(par[u]);
+}
+
+void _union(int a, int b)
+{
+    int x = _find(a);
+    int y = _find(b);
+    if(x!=y)
+    {
+        if(_rank[x]>_rank[y])
+        {
+            par[y] = x;
+        }
+        else
+        {
+            par[x] = y;
+            if(_rank[x]==_rank[y]) _rank[y]++;
+        }
+    }
+}
+
+int main()
+{
+    int t,T;
+    scin(T);
+    RUN_CASE(t,T)
+    {
+        int n;
+        scin(n);
+        for(int i = 1; i <= n; i++) par[i] = i;
+        char ch,ch2[10004];
+        int a,b,cnt = 0, cnt2 = 0;
+        getchar();
+        while(1)
+        {
+            gets(ch2);
+            if(strcmp(ch2,"")==0||feof(stdin)) break;
+            sscanf(ch2,"%c %d %d",&ch,&a,&b);
+            if(ch == 'c')
+            {
+                if(_find(a)!=_find(b))
+                    _union(a,b);
+            }
+            else if(ch=='q')
+            {
+                if(_find(a)==_find(b))
+                    cnt++;
+                else cnt2++;
+            }
+        }
+        if(t>1) pf("\n");
+        pf("%d,%d\n",cnt,cnt2);
+    }
+    return 0;
+}
+
+
+/**
+2
+
+10
+c 1 5
+c 2 7
+q 7 1
+c 3 9
+q 9 6
+c 2 5
+q 7 5
+
+1
+q 1 1
+c 1 1
+q 1 1
+*/
